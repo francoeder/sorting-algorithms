c45fcc5fd84807ac615c59f77abfda901e902d8a
Eder Franco <francoheder@gmail.com>
2024-02-18 16:27:33 -0300

    feat: Initializing project + Adding bubble sort endpoint

---
+++ src/Sorting.Algorithms.API/Controllers/SortingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sorting.Algorithms.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SortingController : ControllerBase
+    {
+        [HttpPut("bubble-sort")]
+        public IActionResult BubbleSort([FromBody] int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        (array[j + 1], array[j]) = (array[j], array[j + 1]);
+                    }
+                }
+            }
+
+            return Ok(array);
+        }
+    }
+}
